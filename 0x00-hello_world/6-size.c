ea0de43bd681d644698694591ce1203338da577a
Aicha BIRAT <aichabirat2001@outlook.fr>
2023-03-16 12:26:01 -0700

    idk6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,14 +9,14 @@
 int main(void)
 {
 	printf("Size of a char: %lu byte(s)", sizeof(char));
-	(/n)
+	/ n
 	printf("Size of an int: %lu byte(s)", sizeof(int));
-	(/n)
+	/ n
 	printf("Size of a long int: %lu byte(s)", sizeof(long int));
-	(/n)
+	/ n
 	printf("Size of a long long int: %lu byte(s)", sizeof(long long int));
-	(/n)
+	/ n
 	printf("Size of a float: %lu byte(s)", sizeof(float));
-	(/n)
+	/ n
 	return (0);
 }
