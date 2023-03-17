c98a38499e2e19794bc29ba05858fc8693b09ee0
Aicha BIRAT <aichabirat2001@outlook.fr>
2023-03-17 13:29:41 -0700

    extra1

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,6 +7,7 @@
 int main(void)
 {
 	int i;
+	int j;
 
 	for (i = 0 ; i < 10 ; i++)
 	{
@@ -16,7 +17,7 @@ int main(void)
 			{
 				putchar(i + '0');
 				putchar(j + '0');
-				if (i + j != '17')
+				if (i + j != 17)
 				{
 					putchar(',');
 					putchar(' ');
