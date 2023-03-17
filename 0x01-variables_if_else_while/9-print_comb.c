e2f6e537445cb338a21ebc48b225c4ec77cbede4
Aicha BIRAT <aichabirat2001@outlook.fr>
2023-03-17 12:55:38 -0700

    again9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,14 +8,12 @@ int main(void)
 {
 	int i;
 
-	for (i = 0 ; i < 10 ; i++)
-	{	
+	for (i = 0 ; i < 10 ; i++)	
 		putchar(i = '0');
-		if (i != 9)
-		{	
-			putchar(',');
-			putchar(' ');
-		}
+	if (i != 9)
+	{	
+		putchar(',');
+		putchar(' ');
 	}
 	putchar('\n');
 	return (0);
