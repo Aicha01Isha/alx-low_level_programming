726ba71a50327bacd0feb32f0047fb721bb1081d
Aicha BIRAT <aichabirat2001@outlook.fr>
2023-03-21 13:19:36 -0700

    task7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_last_digit - function that prints last digit
+ *
+ * @i: parameter to be printed
+ *
+ * Return: k on success
+ */
+int print_last_digit(int i)
+{
+	int k;
+
+	k = i % 10;
+	if (i < 0)
+		k = -k;
+	return (k);
+}
