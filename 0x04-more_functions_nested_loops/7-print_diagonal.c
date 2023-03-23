db0cac8c0120cd630c67d983302e430776bbdbad
Aicha BIRAT <aichabirat2001@outlook.fr>
2023-03-23 14:41:43 -0700

    7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_diagonal - function that prints diagonal
+ *
+ * @n: number to print
+ *
+ * Return: 0 on success
+ */
+void print_diagonal(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i;
+
+		for (i = 1 ; i <= n ; i++)
+		{
+			_putchar('\');
+		}
+		_putchar('\n');
+	}
+}
