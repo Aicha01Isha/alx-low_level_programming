6b74a3e834e8667bc965ce902fe1999049b1cdaf
Aicha BIRAT <aichabirat2001@outlook.fr>
2023-03-23 14:01:35 -0700

    2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul - function that multiples
+ *
+ * @a: parameter to check
+ * @b: parameter to check
+ *
+ * Return: addition of multiplication
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
