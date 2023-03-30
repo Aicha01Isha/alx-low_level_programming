be3c3fd34ab60662dd664d6fa3ac4d6e199d41ad
Aicha BIRAT <aichabirat2001@outlook.fr>
2023-03-30 14:33:11 -0700

    yes4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * reverse_array - function that reverse array of integers
+ *
+ * @a: array
+ *
+ * @n: number of array
+ *
+ * Return: 0 on success
+ */
+void reverse_array(int *a, int n)
+{
+	int i;
+	int t;
+
+	for (i = 0; i < n--; i++)
+	{
+		t = a[i];
+		a[i] = a[n];
+		a[n] = t;
+	}
+}
