c125ec06cc937df8315894ad5a59b83eb27579c6
Aicha BIRAT <aichabirat2001@outlook.fr>
2023-04-28 03:55:34 -0700

    i

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * first - prints a sentence
+ * Return: 0 on success
+ */
+
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
