2920f110029f8b4f62197a7acc3cc9277f15e3a1
Aicha BIRAT <aichabirat2001@outlook.fr>
2023-05-02 04:30:51 -0700

    a

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -29,7 +29,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		a++;
 	}
 	cur = tmp->next;
-	tmp->next = current->next;
+	tmp->next = cur->next;
 	free(cur);
 	return (1);
 }
---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * reverse_listint - function that reverses a linked list
+ * @head: parameter
+ * Return: pointer
+ */
+
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *prv = NULL;
+	listint_t *nxt = NULL;
+
+	while (*head)
+	{
+		nxt = (*head)->nxt;
+		(*head)->nxt = prv;
+		prv = *head;
+		*head = nxt;
+	}
+	*head = prv;
+	return (*head);
+}
